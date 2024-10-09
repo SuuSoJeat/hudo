c573bc1677bc6ba0fc763970fa04d171636c27f1
SuuSoJeat <suusojeat@gmail.com>
2024-10-09 17:29:24 +0700

    ✨ (layout.tsx, app.ts): add app constants

---
+++ src/constants/app.ts
@@ -0,0 +1,7 @@
+export const APP = {
+  NAME: "hudo",
+  TITLE: "HuDo",
+  DESCRIPTION: "Collaborative task management for advancing human knowledge",
+  KEYWORDS: ["todo", "productivity", "collaboration", "research", "academia"],
+  AUTHORS: [{ name: "HuDo Team" }],
+};
